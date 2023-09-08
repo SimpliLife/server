175083ec134a46b2ef73deced54b26a2315e71a0
ayusudi <ayusudi.abc@gmail.com>
2023-09-08 22:05:55 +0700

    Feature list category and diagram diganose

--- controllers/ControllerPraDiagnosa.js
+++ controllers/ControllerPraDiagnosa.js
@@ -1,6 +1,51 @@
+const { Category, Symptom, Sequelize } = require("../models")
+const atCategory = ["id", "category", "icon"]
+const atSymptom = ["id", "title"]
+
 class ControllerPraDiagnosa {
   static root(req, res) {
-    res.send("Ongoing")
+    res.send({
+      "1.": "GET /categories",
+      "2.": "GET /categories/:id/symptoms",
+      "3.": "GET /symptoms/:id",
+    })
+  }
+  static listCategory(req, res, next) {
+    Category.findAll({ attributes: atCategory })
+      .then(data => res.send(data))
+      .catch(err => next(err))
+  }
+  static async listSymptomByCategory(req, res, next) {
+    try {
+      let data = await Category.findByPk(req.params.id, {
+        attributes: atCategory,
+        include: [{ model: Symptom, attributes: atSymptom }]
+      })
+      if (!data) throw { statusCode: 404 }
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  static async symptomById(req, res, next) {
+    try {
+      let data = await Symptom.findByPk(req.params.id, {
+        include: Category,
+        attributes: [
+          "id", "CategoryId", "title", "slug", "ref", "firstQuestion", "q",
+          [Sequelize.col('"Category"."category"'), 'category'],
+          [Sequelize.col('"Category"."icon"'), 'categoryIcon']
+        ],
+        raw: true,
+        nest: true
+      })
+      if (!data) throw { statusCode: 404 }
+      delete data.Category
+      res.send(data)
+    } catch (error) {
+      console.log(error);
+      next(error)
+    }
   }
 }
 
